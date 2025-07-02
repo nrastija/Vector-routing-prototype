d0717af294f12f9415c4c49c00020f6e01dd4684
nrastija <niko.rastija@gmail.com>
2025-07-02 18:38:23 +0200

    feat: add CORS specific middleware for allowing frontend GET and POST methods #3

--- Software/backend/api/endpoints/routing.py
+++ Software/backend/api/endpoints/routing.py
@@ -2,15 +2,27 @@ from fastapi import APIRouter, HTTPException
 from backend.models.route import RouteRequest, RouteResponse
 from typing import List, Optional, Dict, Any
 from backend.core.vector_db import VectorDatabase
-
+from backend.core.osm_data_loader import fetch_osm_data
 router = APIRouter()
-db = VectorDatabase()
+db = VectorDatabase(vector_size=2)
+
 
 # --- Optimal Route ---
 @router.post("/optimal", tags=["Routing"], response_model=RouteResponse)
 def get_optimal_route(data: RouteRequest):
+    start_coords = [data.source_coords[0], data.source_coords[1]]
+    end_coords = [data.dest_coords[0], data.dest_coords[1]]
+
+    route_coords = [start_coords, end_coords]
+
+    osm_result = fetch_osm_data(route_coords)
+    graph = osm_result["graph"]
+    nodes = osm_result["nodes"]
+
+    db.create_embeddings(nodes)
+
     result = db.find_optimal_route(
-        graph=db.graph,
+        graph=graph,
         source_coords=data.source_coords,
         dest_coords=data.dest_coords
     )
--- Software/backend/main.py
+++ Software/backend/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from backend.api.routes import api_router
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+    expose_headers=["*"]  # Exposes all headers
+)
+
 
 app.mount("/routes", StaticFiles(directory="backend/data/routes"), name="routes")
 app.mount("/graphs", StaticFiles(directory="backend/data/OSM graphs"), name="graphs")
