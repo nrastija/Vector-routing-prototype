a1dfda6ff0805d34afc818a7ff81157e6ed9b94d
nrastija <niko.rastija@gmail.com>
2025-07-03 19:36:57 +0200

    fix: add logger in main.py to fix empty terminal that does not display prints and info #2

--- Software/backend/main.py
+++ Software/backend/main.py
@@ -1,26 +1,26 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from backend.api.routes import api_router
 from fastapi.middleware.cors import CORSMiddleware
-
+from backend.api.routes import api_router
+import logging
 app = FastAPI()
 
-origins = [
-    "http://127.0.0.1:3000",
-]
 
+logger = logging.getLogger("uvicorn")
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+# 1. First add CORS middleware (THIS MUST COME FIRST)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
     expose_headers=["*"]  # Exposes all headers
 )
 
-
-app.mount("/routes", StaticFiles(directory="backend/data/routes"), name="routes")
-app.mount("/graphs", StaticFiles(directory="backend/data/OSM graphs"), name="graphs")
+app.mount("/data/routes", StaticFiles(directory="backend/data/routes"), name="routes")
+app.mount("/data/graphs", StaticFiles(directory="backend/data/OSM graphs"), name="graphs")
 
 app.include_router(api_router)
-
